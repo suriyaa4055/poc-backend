2cfee8b29cf56c346fce53e3cc274f5875d5432d
Manoj771vsj <jyothimanoj421@gmail.com>
2023-05-24 11:42:06 +0530

    details model added

--- models/model.go
+++ models/model.go
@@ -1,6 +1,21 @@
 package models
 
 type TestBenchTable struct {
-	HuVersion string
-	HuGen     string
+	HuVersion string `json:"huVersion"`
+	HuGen     string `json:"huGen"`
+}
+
+type Gen20x struct {
+	Version   []string  `json:"version"`
+	SwVersion []float64 `json:"swVersion"`
+}
+
+type NTG struct {
+	Version   []string  `json:"version"`
+	SwVersion []float64 `json:"swVersion"`
+}
+
+type HU struct {
+	Gen20x *Gen20x `json:"gen20x"`
+	NTG    *NTG    `json:"ntg"`
 }
