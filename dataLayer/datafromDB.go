631d057e39052be50f5c966b9900c980c2f8cd62
ashtamikn <ashtamikn@gmail.com>
2023-05-25 11:35:30 +0530

    jh

---
+++ dataLayer/datafromDB.go
@@ -0,0 +1,40 @@
+package datalayer
+
+import (
+	"fmt"
+	"testBench/initializers"
+	"testBench/models"
+)
+
+type DataLayer interface {
+	HeadUnitsFromDB() []models.TestBenchTable
+	GenDataFromDB(gen string) ([]models.Details, error)
+}
+
+type Data struct {
+}
+
+// All the headunits From database
+func (d Data) HeadUnitsFromDB() []models.TestBenchTable {
+
+	var lists []models.TestBenchTable
+	if result := initializers.DB.Find(&lists); result.Error != nil {
+		fmt.Println(result.Error)
+	}
+
+	return lists
+	//json.NewEncoder(w).Encode(lists)
+}
+
+func (d Data) GenDataFromDB(gen string) ([]models.Details, error) {
+
+	var tb []models.Details
+	result := initializers.DB.Where("hu_gen = ?", gen).Find(&tb)
+	if result.Error != nil {
+		//http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return []models.Details{}, result.Error
+	}
+
+	return tb, nil
+	//json.NewEncoder(w).Encode(lists)
+}
